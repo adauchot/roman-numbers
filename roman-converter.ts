ff8e6a48d201373e95059ebb4f0cfa7b641dd3fd
aurelien <aurelien.dauchot@soprism.com>
2024-05-23 17:26:47 +0200

    Using breakdown to convert numbers up to 50

--- roman-converter.ts
+++ roman-converter.ts
@@ -1,13 +1,8 @@
 export abstract class RomanNumberConverter {
     private static arabicToRomanSymbolMap: Map<number, string> = new Map([
         [1, 'I'],
-        [2, 'II'],
-        [3, 'III'],
         [4, 'IV'],
         [5, 'V'],
-        [6, 'VI'],
-        [7, 'VII'],
-        [8, 'VIII'],
         [9, 'IX'],
         [10, 'X'],
         [40, 'XL'],
@@ -15,7 +10,15 @@ export abstract class RomanNumberConverter {
     ]);
 
     static convert(number: number) {
-        return this.arabicToRomanSymbolMap.get(number);
+        let leftover = number;
+        let result = '';
+        while (leftover > 0) {
+            const maxSymbolValue = RomanNumberConverter.breakdownMaxSymbolValue(leftover, Array.from(RomanNumberConverter.arabicToRomanSymbolMap.keys()));
+            result += RomanNumberConverter.arabicToRomanSymbolMap.get(maxSymbolValue);
+            leftover -= maxSymbolValue;
+        }
+
+        return result;
     }
 
     static breakdownMaxSymbolValue(numberToTest: number, listOfArabicNumbers: number[]) {
