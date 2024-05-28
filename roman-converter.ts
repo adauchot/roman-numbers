b904dae2d4b047d306f902af0b737d9010c56df8
aurelien <aurelien.dauchot@soprism.com>
2024-05-28 08:54:08 +0200

    Test up to 500 ok

--- roman-converter.ts
+++ roman-converter.ts
@@ -8,7 +8,8 @@ export abstract class RomanNumberConverter {
         [40, 'XL'],
         [50, 'L'],
         [90, 'XC'],
-        [100, 'C']
+        [100, 'C'],
+        [400, 'CD']
     ]);
 
     static convert(number: number) {
